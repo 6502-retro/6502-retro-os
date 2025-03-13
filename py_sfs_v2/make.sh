40dc4175139ec021928460641d28559f6ee875b9
ProductionDave <kiwilathams@gmail.com>
2025-03-13 21:09:53 +1300

    update sdcard maker

--- py_sfs_v2/make.sh
+++ py_sfs_v2/make.sh
@@ -5,9 +5,13 @@ rm sdcard.img
 mkdir files
 cd files
 find ../../apps/ -name "*.com" -exec cp {} ./ \;
+find ../../../vgm/ -name "*.vgm" -exec cp {} ./ \;
+find ../../basic_prg/ -name "*.bas" -exec cp {} ./ \;
 
 ../cli.py new -i ../sdcard.img
 ../cli.py format -i ../sdcard.img
 for file in *.com; do ../cli.py cp -i ../sdcard.img -s $file -d a://$file; done
+for file in *.vgm; do ../cli.py cp -i ../sdcard.img -s $file -d e://$file; done
+for file in *.bas; do ../cli.py cp -i ../sdcard.img -s $file -d d://$file; done
 cd ..
 ./cli.py installos -i sdcard.img -o ../build/rom.raw
